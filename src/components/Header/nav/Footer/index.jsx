923f14865fddd567e20dfb672a214f1b89c4edaa
3TERRRNITY <qertisww@gmail.com>
2023-10-22 20:21:42 +0300

    create header

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./style.module.scss";
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+      <a>Telegram</a>
+      <a>Instagram</a>
+    </div>
+  );
+}
