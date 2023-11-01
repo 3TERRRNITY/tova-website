86a9fc61a0931d78836c8d539a844567e652f446
3TERRRNITY <qertisww@gmail.com>
2023-11-01 19:26:26 +0300

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,45 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import styles from "../styles/hero.module.scss";
 import Navbar from "../components/Navbar/Navbar";
 import Header from "../components/Header/Header";
 import { IMAGES, PROJECTS } from "../constants/constants";
 import gsap from "gsap";
-import Image from "next/image";
-import { motion, useAnimation } from "framer-motion";
-function Card({ title, description }: { title: string; description: string }) {
-  const controls = useAnimation();
-  const [isHovered, setIsHovered] = useState(false);
+import { ScrollTrigger } from "gsap/all";
+import Card from "../components/Card/Card";
 
-  const handleHover = () => {
-    controls.start({
-      opacity: 1,
-      x: 0,
-    });
-    setIsHovered(true);
-  };
-
-  const handleUnhover = () => {
-    controls.start({
-      opacity: 0,
-      x: -100,
-    });
-    setIsHovered(false);
-  };
-  return (
-    <motion.div
-      className={styles.projects__card}
-      onHoverStart={handleHover}
-      onHoverEnd={handleUnhover}
-    >
-      <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 0.25 }}
-        animate={controls}
-        className={styles.projects__card__yearContainer}
-      >
-        <Image src="/right-arrow.svg" alt="arrow" width={41} height={21} />
-        <div className="year">2023</div>
-      </motion.div>
-      <motion.div className={styles.projects__card__container}>
-        <motion.h2
-          className={styles.projects__card__title}
-          initial={{ y: 0 }}
-          transition={{ duration: 0.25 }}
-          animate={{
-            y: isHovered ? 200 : 0,
-          }}
-        >
-          {title}
-        </motion.h2>
-        <motion.p
-          initial={{ opacity: 0 }}
-          transition={{ duration: 0.4 }}
-          className={styles.projects__card__description}
-          animate={{
-            opacity: isHovered ? 1 : 0,
-          }}
-        >
-          {description}
-        </motion.p>
-      </motion.div>
-    </motion.div>
-  );
-}
 export default function Home() {
   const firstContainer = useRef(null);
   const secondContainer = useRef(null);
+  const slider = useRef(null);
   let xPercent = 0;
   let direction = -1;
 
-  const animation = () => {
-    if (xPercent <= -100) {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
       xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
     }
     gsap.set(firstContainer.current, { xPercent: xPercent });
     gsap.set(secondContainer.current, { xPercent: xPercent });
-    xPercent += 0.05 * direction;
-    requestAnimationFrame(animation);
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
   };
   const brandsContainer = IMAGES.map(({ img, key }) => (
     <div className={styles.slider__item} key={key}>
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useRef, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import Image from "next/image";
+import styles from "../../styles/hero.module.scss";
+
+export default function Card({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  const controls = useAnimation();
+  const [isHovered, setIsHovered] = useState(false);
+  const [cardHeight, setCardHeight] = useState<number>(0);
+  const heightRef = useRef(null as null | HTMLDivElement);
+
+  const handleHover = () => {
+    controls.start({
+      opacity: 1,
+      x: 0,
+    });
+    setIsHovered(true);
+  };
+
+  const handleUnhover = () => {
+    controls.start({
+      opacity: 0,
+      x: -100,
+    });
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    setCardHeight(heightRef.current?.clientHeight ?? 0);
+  }, [heightRef]);
+  return (
+    <motion.div
+      className={styles.projects__card}
+      onHoverStart={handleHover}
+      onHoverEnd={handleUnhover}
+      ref={heightRef}
+    >
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.25 }}
+        animate={controls}
+        className={styles.projects__card__yearContainer}
+      >
+        <Image src="/right-arrow.svg" alt="arrow" width={41} height={21} />
+        <div className="year">2023</div>
+      </motion.div>
+      <motion.div
+        className={styles.projects__card__container}
+        initial={{ y: 0 }}
+        transition={{ duration: 0.25 }}
+        animate={{
+          y: isHovered ? cardHeight - 150 : 0,
+        }}
+      >
+        <motion.h2 className={styles.projects__card__title}>{title}</motion.h2>
+        <motion.p
+          initial={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className={styles.projects__card__description}
+          animate={{
+            opacity: isHovered ? 1 : 0,
+          }}
+        >
+          {description}
+        </motion.p>
+      </motion.div>
+    </motion.div>
+  );
+}
