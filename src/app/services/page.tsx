56de464674c3a677abe2420888406c1defb2c629
3TERRRNITY <qertisww@gmail.com>
2023-11-12 18:34:59 +0300

    services page layout

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,37 +1,53 @@
 "use client";
+
 import React from "react";
 import styles from "../../styles/services.module.scss";
 import Header from "../../components/Header/Header";
 import { SERVICE, SERVICES } from "../../constants/constants";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import { Separator } from "../../components/Separator/Separator";
 
 const Services = () => {
   return (
-    <div className={styles.services}>
+    <>
       <Header />
-      <div className={styles.services__title}>услуги</div>
-      <div className={styles.services__preview}>
-        <div className={styles.services__preview__title}>превью</div>
-        <ul className={styles.services__preview__links}>
-          {SERVICES.map((serv) => (
-            <li className={styles.services__preview__links_link}>
-              <a href={`#${serv.href}`}>{serv.title}</a>
+      <div className={styles.services}>
+        <div className={styles.services__title}>услуги</div>
+        <Separator />
+        <div className={styles.services__preview}>
+          <div className={styles.services__preview__title}>превью</div>
+          <ul className={styles.services__preview__links}>
+            <li className={styles.services__preview__links_title}>
+              название услуги
             </li>
+            <Separator />
+            {SERVICES.map((serv) => (
+              <React.Fragment key={serv.title}>
+                <li className={styles.services__preview__links_link}>
+                  <a href={`#${serv.href}`}>{serv.title}</a>
+                </li>
+                <Separator />
+              </React.Fragment>
+            ))}
+          </ul>
+        </div>
+        <Separator />
+        <div className={styles.services__projects}>
+          {SERVICE.map(({ title, description, points, href }) => (
+            <React.Fragment key={title}>
+              <ServiceCard
+                key={title}
+                title={title}
+                description={description}
+                points={points}
+                href={href}
+              />
+              <Separator />
+            </React.Fragment>
           ))}
-        </ul>
+        </div>
       </div>
-      <div className={styles.services__projects}>
-        {SERVICE.map(({ title, description, points, href }) => (
-          <ServiceCard
-            key={title}
-            title={title}
-            description={description}
-            points={points}
-            href={href}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
