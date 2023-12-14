3e97ed84eab488e18d02dfd5a1ba44c03e471483
3TERRRNITY <qertisww@gmail.com>
2023-12-14 14:11:30 +0300

    delete random shuffle

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -21,14 +21,6 @@ const Projects = () => {
   const handleOptionClick = (option: string) => {
     setSelectedOption(option);
   };
-  function shuffleArray(array: any[]) {
-    const shuffled = array.slice();
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  }
 
   // Эффект, отслеживающий изменения selectedOption
   useEffect(() => {
@@ -44,7 +36,7 @@ const Projects = () => {
 
     // Перемешивание массива, если он не пустой
     if (newFilteredProjects.length > 0) {
-      const shuffledProjects = shuffleArray(newFilteredProjects);
+      const shuffledProjects = newFilteredProjects;
       setFilteredProjects(shuffledProjects);
     } else {
       setFilteredProjects(newFilteredProjects);
