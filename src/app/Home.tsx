01e9dca29cd863c35b373f38feb4535609b13311
3TERRRNITY <qertisww@gmail.com>
2024-01-11 13:21:39 +0300

    Revert "cursor fix"
    This reverts commit eab988ab0df25703b9d9cb15f4bc4f6d9754974c.

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -8,12 +8,39 @@ import Footer from "../components/Footer/Footer";
 import Link from "next/link";
 import Image from "next/image";
 import Slider from "../components/Slider/Slider";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
-import ServiceCard from "../components/ServiceCard/ServiceCard";
-import { ServicesCard } from "../components/ServicesCard";
 
 export default function Home() {
+  const redirectToServicePage = (serviceTitle: string) => {
+    const url = `/services#${serviceTitle}`;
+    window.location.href = url;
+  };
+  const [hoverPosition, setHoverPosition] = useState<{
+    [key: number]: { x: number; y: number };
+  }>({});
+
+  const handleMouseMove = (
+    index: number,
+    e: React.MouseEvent<HTMLDivElement>
+  ) => {
+    const boundingRect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - boundingRect.left;
+    const y = e.clientY - boundingRect.top;
+    setHoverPosition((prevPositions) => ({
+      ...prevPositions,
+      [index]: { x, y },
+    }));
+  };
+
+  const resetHoverPosition = (index: number) => {
+    setHoverPosition((prevPositions) => {
+      const updatedPositions = { ...prevPositions };
+      delete updatedPositions[index];
+      return updatedPositions;
+    });
+  };
+
   return (
     <div className={commonStyles.mainPageBody}>
       <div className={styles.hero}>
@@ -38,7 +65,47 @@ export default function Home() {
       <Slider />
       <div className={styles.services}>
         <div className={styles.services__title}>услуги</div>
-        <ServicesCard />
+        <div className={styles.services__serviceGrid}>
+          {SERVICES.map((service, index) => (
+            <Link key={service.title} href={`/services#${service.href}`}>
+              <motion.div
+                className={styles.services__card}
+                onMouseMove={(e) => handleMouseMove(index, e)}
+                onMouseLeave={() => resetHoverPosition(index)}
+                style={{ position: "relative" }}
+              >
+                <div className={styles.services__card__info}>
+                  <div className={styles.services__card__info_title}>
+                    {service.title}
+                  </div>
+                  {hoverPosition[index]?.x !== undefined &&
+                    hoverPosition[index]?.y !== undefined && (
+                      <motion.div
+                        className={styles.services__card__cursor}
+                        style={{
+                          left: hoverPosition[index].x + "px",
+                          top: hoverPosition[index].y + "px",
+                          position: "absolute",
+                        }}
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0 }}
+                        transition={{ duration: 0.2, ease: "easeInOut" }}
+                      ></motion.div>
+                    )}
+                </div>
+                <span className={styles.services__card__price}>
+                  {service.price}
+                </span>
+                <img
+                  src={service.image}
+                  alt="service image"
+                  className={styles.services__card__image}
+                />
+              </motion.div>
+            </Link>
+          ))}
+        </div>
         {/* DEV: старый вариант без неонового курсора */}
         {/* <div className={styles.services__serviceGrid}>
           {SERVICES.map((service) => (
