c3cd1864cd199bbd87b8cef597206746d99c7ca8
3TERRRNITY <qertisww@gmail.com>
2024-04-22 13:56:36 +0300

    video services block

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Form from "../../components/Form/Form";
 import Header from "../../components/Header/Header";
 import styles from "../../styles/video.module.scss";
 import SplineAnim from "./spline";
 import { motion, useAnimation } from "framer-motion";
+import { Separator } from "../../components/Separator/Separator";
+import { title } from "process";
 
 const video = () => {
   const control = useAnimation();
@@ -35,6 +37,28 @@ const video = () => {
 
     animateWords();
   }, []);
+  const services = [
+    {
+      title: "реклама",
+      href: "/video/ad.png",
+      link: "/video#ad",
+    },
+    {
+      title: "фильмы и телевидение",
+      href: "/video/films.png",
+      link: "/video#films",
+    },
+    {
+      title: "музыкальные клипы",
+      href: "/video/clips.png",
+      link: "/video#clips",
+    },
+    {
+      title: "всестороннее производство",
+      href: "/video/prod.png",
+      link: "/video#prod",
+    },
+  ];
   return (
     <>
       <div className={styles.pageContainer}>
@@ -117,6 +141,26 @@ const video = () => {
             </div>
           </div>
         </div>
+        <Separator />
+        <div className={styles.body}>
+          <div className={styles.text}>
+            мы предоставляем комплексные услуги видеопродакшна, охватывающие всё
+            от съемки реклам и музыкальных клипов до создания сериалов и
+            разнообразного видеоконтента. В нашей команде — профессионалы с
+            глубокими знаниями и опытом в области рекламы, кинематографа и
+            медиа-производства. Мы гордимся нашей способностью превращать
+            творческие замыслы в реальность, создавая визуально захватывающие и
+            эмоционально мощные произведения.
+          </div>
+          <div className={styles.services}>
+            <div className={styles.services_title}>УСЛУГИ</div>
+            <div className={styles.services_grid}>
+              {services.map(({ title, href, link }) => (
+                <HoverableTitle title={title} href={href} link={link} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
       <div className={styles.whitePageContainer}>
         <Form />
@@ -127,3 +171,75 @@ const video = () => {
 };
 
 export default video;
+
+const HoverableTitle = ({
+  title,
+  href,
+  link,
+}: {
+  title: string;
+  href: string;
+  link: string;
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <a href={link} style={{ textDecoration: "none", color: "inherit" }}>
+      <div
+        style={{
+          position: "relative",
+          textAlign: "center",
+          cursor: "pointer",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+          width: "100%",
+          height: "100%",
+        }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div style={{ zIndex: "100", width: "217px" }}>{title}</div>
+
+        {isHovered && (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              zIndex: "100",
+              textDecoration: "underline",
+              justifySelf: "end",
+            }}
+          >
+            Перейти
+          </motion.span>
+        )}
+
+        {isHovered && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            style={{
+              position: "absolute",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              background: `url(${href}) no-repeat center center`,
+              backgroundSize: "contain",
+              zIndex: "1",
+              filter: "revert",
+              opacity: "0.7",
+            }}
+          />
+        )}
+      </div>
+    </a>
+  );
+};
