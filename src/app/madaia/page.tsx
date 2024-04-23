389d52905f21c571d4f97246ca2b1eefa634fea2
3TERRRNITY <qertisww@gmail.com>
2024-04-23 13:09:06 +0300

    fix

--- src/app/madaia/page.tsx
+++ src/app/madaia/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/madaia.module.scss";
 import commonStyles from "../../styles/common.module.scss";
